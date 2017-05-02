efe0a4c17b1176fc4209fe8314572d99a6b2a45e
Zulus <danil.kazim.99@gmail.com>
2017-05-02 18:03:00 +0300

    fix image loading

--- src/FileBrowserTutor/FileBrowserWindow.cpp
+++ src/FileBrowserTutor/FileBrowserWindow.cpp
@@ -6,15 +6,13 @@ FileBrowserWindow::FileBrowserWindow(QWidget *parent)
   ui->setupUi(this);
   QThreadPool::globalInstance()->setMaxThreadCount(1);
   model = new QFileSystemModel(this);
-  model->setRootPath("\\");
-
+  model->setRootPath("/");
   filesmodel = new QStandardItemModel(this);
   ui->treeView->setModel(model);
   ui->listView->setModel(filesmodel);
-  connect(this, SIGNAL(UpdateItem(int, QImage)), SLOT(setThumbs(int, QImage)));
-
+  ui->treeView->setRootIndex(model->index("/home/zulus/Projects/progbase3"));
   ui->treeView->header()->setSectionResizeMode(QHeaderView::ResizeToContents);
-
+  connect(this, SIGNAL(UpdateItem(int, QImage)), SLOT(setThumbs(int, QImage)));
   running = false;
 }
 
@@ -22,13 +20,9 @@ FileBrowserWindow::~FileBrowserWindow() { delete ui; }
 
 void FileBrowserWindow::on_treeView_clicked(const QModelIndex &index) {
   filesmodel->clear();
-
   running = false;
-
   thread.waitForFinished();
-
   QDir dir(model->filePath(ui->treeView->currentIndex()));
-
   QFileInfoList filesList = dir.entryInfoList(QStringList() << "*.jpg"
                                                             << "*.jpeg"
                                                             << "*.tif"
@@ -36,25 +30,19 @@ void FileBrowserWindow::on_treeView_clicked(const QModelIndex &index) {
                                                             << "*.gif"
                                                             << "*.bmp",
                                               QDir::Files);
-
   int filesCount = filesList.size();
-
   QPixmap placeholder = QPixmap(ui->listView->iconSize());
   placeholder.fill(Qt::gray);
-
   for (int i = 0; i < filesCount; i++)
     filesmodel->setItem(
         i, new QStandardItem(QIcon(placeholder), filesList[i].baseName()));
-
   running = true;
-
   thread = QtConcurrent::run(this, &FileBrowserWindow::List, filesList,
                              ui->listView->iconSize());
 }
 
 void FileBrowserWindow::List(QFileInfoList filesList, QSize size) {
   int filesCount = filesList.size();
-
   for (int i = 0; running && i < filesCount; i++) {
     QImage originalImage(filesList[i].filePath());
     if (!originalImage.isNull()) {
