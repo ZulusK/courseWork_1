3ab824d40f49981803ef98493ff6d055716573e3
Zulus <danil.kazim.99@gmail.com>
2017-05-02 03:34:06 +0300

    fix fileBrowser

--- src/FileBrowserTutor/FileBrowserWindow.cpp
+++ src/FileBrowserTutor/FileBrowserWindow.cpp
@@ -1,13 +1,25 @@
 #include "FileBrowserWindow.h"
 #include "ui_FileBrowserWindow.h"
-
+#include <iostream>
 FileBrowserWindow::FileBrowserWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::FileBrowserWindow) {
   ui->setupUi(this);
-  QString sPath = "~/";
+  QString sPath = "/home/zulus";
   dirModel = new QFileSystemModel(this);
+  dirModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
   dirModel->setRootPath(sPath);
   ui->dirTree->setModel(dirModel);
+
+  fileModel = new QFileSystemModel(this);
+  fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
+  fileModel->setRootPath(sPath);
+  ui->dirList->setModel(fileModel);
 }
 
 FileBrowserWindow::~FileBrowserWindow() { delete ui; }
+
+void FileBrowserWindow::on_dirTree_clicked(const QModelIndex &index) {
+  QString sPath = dirModel->fileInfo(index).absoluteFilePath();
+  std::cout << sPath.toStdString() << std::endl;
+  ui->dirList->setRootIndex(fileModel->setRootPath(sPath));
+}
--- src/FileBrowserTutor/FileBrowserWindow.h
+++ src/FileBrowserTutor/FileBrowserWindow.h
@@ -14,6 +14,9 @@ public:
   explicit FileBrowserWindow(QWidget *parent = 0);
   ~FileBrowserWindow();
 
+private slots:
+    void on_dirTree_clicked(const QModelIndex &index);
+
 private:
   Ui::FileBrowserWindow *ui;
   QFileSystemModel *dirModel;
